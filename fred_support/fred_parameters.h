3617fed337ade4001a7ddfd3162cc97eb973993d
Marco Pagani <marco.pag@outlook.com>
2019-01-08 18:15:47 +0100

    Initial commit

---
+++ fred_support/fred_parameters.h
@@ -0,0 +1,38 @@
+/*
+ * Fred for Linux. Experimental support.
+ *
+ * Copyright (C) 2018, Marco Pagani, ReTiS Lab.
+ * <marco.pag(at)outlook.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+*/
+
+#ifndef __PARAMETERS_H__
+#define __PARAMETERS_H__
+
+//-------------------------------------------------------------------------------
+
+#define LIST_SOCK_PATH			"/tmp/fred_sock"
+
+#define FRED_PATH				"/fredsys/"
+
+//-------------------------------------------------------------------------------
+
+#define MAX_HW_TASKS			128
+
+#define HW_OP_ARGS_SIZE			8
+
+#define MAX_DATA_BUFFS			HW_OP_ARGS_SIZE
+
+//-------------------------------------------------------------------------------
+
+#define MAX_NAMES				128
+
+#define MAX_PATH				1024
+
+//-------------------------------------------------------------------------------
+
+#endif // __PARAMETERS_H__
